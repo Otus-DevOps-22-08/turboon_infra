8762dddbafd4e8e0cd18e3f4de3f975ead8f5b3f
turboon@inbox.ru <turboon@inbox.ru>
2022-11-16 12:05:04 +0400

    install_modgodb.sh fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,7 +2,7 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt update
-apt install mongodb-org
+apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
 systemctl status mongod
