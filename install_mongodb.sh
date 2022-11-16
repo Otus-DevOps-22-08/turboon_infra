08c9bbcec268c30e7b068bc533f94fe0827c4fab
turboon@inbox.ru <turboon@inbox.ru>
2022-11-16 12:41:54 +0400

    Added sudo to scripts

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt update
-apt install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
-systemctl status mongod
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
