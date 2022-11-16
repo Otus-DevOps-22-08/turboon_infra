fd0a94155ebc3a01e2b68e03486351d153443b2c
turboon@inbox.ru <turboon@inbox.ru>
2022-11-16 12:50:20 +0400

    Added apt-transport-https and ca-certs installation FIX

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-auso apt-get install -y apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
