53109bf0b03927e1bed10b82020d4e30c86e6a0a
turboon@inbox.ru <turboon@inbox.ru>
2022-12-03 03:47:44 +0400

    Downgrade to TF 0.12

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   service_account_key_file = var.service_account_key_file
-  version = "~> 0.35"
+  version                  = "~> 0.35"
 }
 
 
