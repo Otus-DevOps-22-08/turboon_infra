e07ed38c2f0bbef13a0cf28f908ab58ab8d9093b
turboon@inbox.ru <turboon@inbox.ru>
2022-12-05 02:28:39 +0400

    Remove LB creation due to terraform crash

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,6 @@ output "external_ip_addresses" {
 #output "external_ip_address_app2" {
 #	value = yandex_compute_instance.app2.network_interface.0.nat_ip_address
 #}
-output "alb_ip_address" {
-  value = yandex_alb_load_balancer.reddit-app-balancer.listener.0.endpoint.0.address.0.external_ipv4_address.0
-}
+#output "alb_ip_address" {
+#  value = yandex_alb_load_balancer.reddit-app-balancer.listener.0.endpoint.0.address.0.external_ipv4_address.0
+#}
